ff14250de585d44794cf942762e3a959fc5d0425
Unknown <eebolue@gmail.com>
2017-11-07 21:54:46 -0500

    Setup RxSwift pods
    - RxSwift
    - RxCocoa

--- Podfile
+++ Podfile
@@ -2,11 +2,14 @@
 # platform :ios, '9.0'
 
 target 'FirebasePractice' do
-  use_frameworks!
 
-  pod 'Firebase/Core'	
-  pod 'Firebase/Auth'	
-  pod 'Firebase/Database'
-  pod 'Firebase/Messaging'	
+    use_frameworks!
+
+    pod 'RxSwift', '~> 4.0'
+    pod 'RxCocoa', '~> 4.0'
+    pod 'Firebase/Core'
+    pod 'Firebase/Auth'
+    pod 'Firebase/Database'
+    pod 'Firebase/Messaging'
   
 end
--- Pods/Target Support Files/Pods-FirebasePractice/Pods-FirebasePractice-frameworks.sh
+++ Pods/Target Support Files/Pods-FirebasePractice/Pods-FirebasePractice-frameworks.sh
@@ -105,6 +105,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
@@ -112,6 +114,8 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
